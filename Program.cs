7ab3d71d0fd4a533c7a4920290fc30cff07b7f28
Jéssica <jessicaspf@gmail.com>
2024-01-08 21:17:29 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+    class Program
+    {
+        static void Main()
+        {
+           
+            Nokia nokia = new Nokia("1111", "nokia 3", "2222", 64);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("Instagram");
+
+
+            Iphone iphone = new Iphone("2222", "iPhone 11", "3333", 128);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("TikTok");
+        }
+    }
